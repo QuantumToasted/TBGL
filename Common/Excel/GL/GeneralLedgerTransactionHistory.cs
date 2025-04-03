d0fa319b938b59345cc5cff17c93ca2059aa0594
Daniel Jarski <danieljarski@gmail.com>
2025-04-03 15:00:59 -0500

    Additional prettifying of transaction details
    Changed the usability of the details button based on if there is transaction history for a G/L account

--- Common/Excel/GL/GeneralLedgerTransactionHistory.cs
+++ Common/Excel/GL/GeneralLedgerTransactionHistory.cs
@@ -8,11 +8,17 @@ namespace TBGL.Common;
 
 public class GeneralLedgerTransactionHistory(IXLRange range)
 {
+    private int? _transactionCount;
+    
     public GeneralLedgerAccountMetadata Metadata { get; } = GeneralLedgerAccountMetadata.Parse(range.FirstCell().GetString());
 
     public decimal StartingBalance { get; } = range.FirstRow().LastCell().GetValue<decimal>();
 
     public decimal EndingBalance { get; } = range.LastRow().LastCell().GetValue<decimal>();
+
+    public bool HasTransactions => (_transactionCount ??= EnumerateTransactions(false).Count()) > 0;
+
+    public string ButtonText => HasTransactions ? "View" : "None";
 
     public IEnumerable<GeneralLedgerTransaction> EnumerateTransactions(bool includeBalances = true)
     {
