f98fbec100863a95d2318524ff9abebf029bd792
Daniel Jarski <30324210+QuantumToasted@users.noreply.github.com>
2025-04-05 11:06:50 -0500

    Changed from absolute "A1" style references
    ...and added a reference to the reporting book

--- Common/Excel/XLWorksheetLoadResult.cs
+++ Common/Excel/XLWorksheetLoadResult.cs
@@ -5,19 +5,17 @@ namespace TBGL.Common;
 
 public abstract class XLWorksheetLoadResult(string path, IXLWorksheet worksheet) : IDisposable
 {
-    private const string PROPERTY_METADATA_CELL = "B6";
-    private const string START_DATE_CELL = "B4";
-    private const string END_DATE_CELL = "B5";
-
     public string Path { get; } = path;
 
     public IXLWorksheet Worksheet { get; } = worksheet;
     
-    public PropertyMetadata Property { get; } = PropertyMetadata.Parse(worksheet.Cell(PROPERTY_METADATA_CELL).GetString());
+    public PropertyMetadata Property { get; } = PropertyMetadata.Parse(worksheet.Cell(6, 2).GetString());
     
-    public DateOnly StartDate { get; } = DateOnly.Parse(worksheet.Cell(START_DATE_CELL).GetString());
+    public DateOnly StartDate { get; } = DateOnly.Parse(worksheet.Cell(4, 2).GetString());
     
-    public DateOnly EndDate { get; } = DateOnly.Parse(worksheet.Cell(END_DATE_CELL).GetString());
+    public DateOnly EndDate { get; } = DateOnly.Parse(worksheet.Cell(5, 2).GetString());
+
+    public string ReportingBook { get; } = worksheet.Cell(3, 2).GetString();
 
     public void Dispose()
     {
